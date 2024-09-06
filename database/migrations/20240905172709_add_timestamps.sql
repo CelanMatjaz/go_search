356c52ad8f4dbf24f05a845db759ea5f07d7ade8
CelanMatjaz <matjazcelangithub@gmail.com>
2024-09-06 09:55:32 +0200

    update defaults for timestamps

--- database/migrations/20240905172709_add_timestamps.sql
+++ database/migrations/20240905172709_add_timestamps.sql
@@ -1,16 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE users
-ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT NOW();
+ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc'),
+ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc');
 
 ALTER TABLE resumes
-ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT NOW();
+ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc'),
+ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc');
 
 ALTER TABLE job_listings
-ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT NOW();
+ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc'),
+ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT (now() at time zone 'utc');
 -- +goose StatementEnd
 
 -- +goose Down

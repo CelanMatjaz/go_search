b8535abeb7136c53d91ba922e203fcde055a8072
Matjaž Čelan <matjazcelangithub@gmail.com>
2024-09-09 21:18:14 +0200

    add migration utility scripts

---
+++ database/cmd/create_migration/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Provide a migration name")
+	}
+
+	godotenv.Load("dev.env")
+	connectionString := os.Getenv("POSTGRES_URL")
+
+	cmd := exec.Command("goose", "-dir", "migrations", "postgres", connectionString, "create", os.Args[1], "sql")
+	output, _ := cmd.CombinedOutput()
+	print(string(output))
+}
